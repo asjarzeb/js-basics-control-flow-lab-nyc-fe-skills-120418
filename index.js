65e99ce1c45ad51dcf80b0bf6b6fb327c4ae03f8
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-11 23:59:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet (somedistance) {
-  if (somedistance >= 400) {
+function scuberGreetingForFeet (someDistance) {
+  if (someDistance >= 400) {
     console.log ('This one is on me!')
   }
 }
