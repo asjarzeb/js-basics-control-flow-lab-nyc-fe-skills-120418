ef4a758a90f77dced33fbb54d672a79b18b0af7c
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-12 00:23:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,13 @@ function ternaryCheckCity(someCity) {
 }
 
 
+describe('ternaryCheckCity()', function () {
+    it('returns "Ok, sounds good." when the city is NYC', function () {
+      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+    });
+
+    it('should return "No go." if the destination city is not NYC', function () {
+      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+    });
+  });
 
