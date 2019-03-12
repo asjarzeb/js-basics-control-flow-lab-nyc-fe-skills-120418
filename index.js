bce5c33ee8607583f81fa7548352c5290b5469e9
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-12 00:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ function scuberGreetingForFeet (someDistance) {
   }
 }
 
+function scuberGreetingForFeet (someDistance) {
+  if (someDistance >2000) {
+    return ('I will gladly take your thirty bucks.')
+  }
+}
 
 
+it('charges 30 dollars for a distance over 2000 feet', function () {
+      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+    });
 
+    it('does not allow rides over 2500 feet', function () {
+      expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
+    });
+  });
 
 
