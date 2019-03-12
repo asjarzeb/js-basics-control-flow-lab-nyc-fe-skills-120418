51ba7704c710b4b691dcc0c1fb7f476cfc760d1a
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-12 00:22:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,20 @@ function scuberGreetingForFeet (someDistance) {
 }
 
 
-function ternaryCheckCity()
+function ternaryCheckCity(someCity) {
+  if (somecity = 'NYC') {
+    return ('Ok, sounds good.')
+  }
+}
 
+
+
+describe('ternaryCheckCity()', function () {
+    it('returns "Ok, sounds good." when the city is NYC', function () {
+      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+    });
+
+    it('should return "No go." if the destination city is not NYC', function () {
+      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+    });
+  });
